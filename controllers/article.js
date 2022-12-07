b775bfd5c94e72052f00e1d96a6fa5a06d6dca79
Diadia Diouf <dioufmarahib1@gmail.com>
2022-12-07 11:17:06 +0000

    update

--- controllers/article.js
+++ controllers/article.js
@@ -11,7 +11,6 @@ exports.createArticle = (req,res)=>{
     // .catch((error)=>{
     //     return res.status(501).json(error);
     // })
-    
     res.render("article/addORedit", {
         viewTitle: "Formulaire d'ajout d'un article"
     });
@@ -21,7 +20,9 @@ exports.getAllArticle=(req,res)=>{
     Article.find()
     .then((article)=>{
         //return res.status(200).json(article);
-        return res.render("article/list", { list : article });
+        return res.render("article/list", {
+            list : article.map(article=>article.toJSON())
+        });
     })
     .catch((error)=>{
         return res.status(501).json(error);
@@ -56,53 +57,34 @@ exports.insertArticle=(req,res)=>{
 }
 //Misse à jour d'un Article
 exports.updateArticle=(req,res)=>{
-    const article= new Article();
-    article.id=req.params._id;
-    article.titre = req.body.titre;
-    article.resumer = req.body.resumer;
-    article.contenu = req.body.contenu;
-    article.auteur = req.body.auteur;
-    article.date_creation = req.body.date_creation;
-    article.date_mise_jour = req.body.date_mise_jour;
-    Article.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true })
-    .then((article)=>{
-        return res.status(200).json(article);
-    })
-    .catch((error)=>{
-        return res.status(501).json(error);
-    })
-    // (err, doc) => {
-    //     if (!err) {
-    //         //res.redirect('article/list'); 
-    //         res.status(200).json(Article);
-    //     }
-    //     else {
-    //         if (doc) {
-    //             res.render("article/addORedit", {
-    //                 viewTitle: 'Modification Article',
-    //                 article: req.body
-    //             });
-    //         }
-    //         else
-    //             console.log('Erreur de Modification : ' + err);
-    //     }
-    // });
-}
+    Article.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true },(err, doc) => {
+        if (!err) {
+            res.redirect("article/list"); 
+            //res.status(200).json(Article);
+        }
+        else {
+            if (doc) {
+                res.render("article/addORedit", {
+                    viewTitle: 'Modifier un Article',
+                    article: req.body
+                });
+            }
+            else{
+                console.log('Erreur de Modification : ' + err);
+            }
+        }
+    });
+} 
 //Rechercher et Mettre à jour
 exports.getIDArticle=(req, res) => {
-    Article.find({_id:req.params._id})
-    .then((Article)=>{
-        return res.status(200).json(Article);
+    Article.findById(req.params.id, (err, doc) => {
+        if (!err) {
+            res.render("article/addORedit", { 
+                viewTitle: 'Modifier un Article',
+                article: req.body
+            });
+        }
     })
-    .catch((error)=>{
-        return res.status(501).json(error);
-    })
-        // if (!err) {
-        //     res.render("article/addORedit", {
-        //         viewTitle: "Modifier un article",
-        //         article: doc
-        //     });
-        // }
 }
 //Supprrimer un Article
 exports.deleteArticle=(req, res) => {
--- routers/article.js
+++ routers/article.js
@@ -4,9 +4,15 @@ const articleCtrl=require('../controllers/article')
 
 route.get('/', articleCtrl.createArticle);
 route.get('/list', articleCtrl.getAllArticle);
-route.post('/', articleCtrl.insertArticle);
-route.get('/list/:id',articleCtrl.getIDArticle);
-route.get('/update/:id', articleCtrl.updateArticle);
+//route.post('/', articleCtrl.insertArticle);
+route.post('/', (req, res) => {
+    if (req.body._id == '')
+       articleCtrl.insertArticle(req, res);
+    else
+       articleCtrl.updateArticle(req, res);
+});
+route.get('/:id',articleCtrl.getIDArticle);
+//route.post('/', articleCtrl.updateArticle);
 route.delete('/:id',articleCtrl.deleteArticle);
 
 module.exports=route;
